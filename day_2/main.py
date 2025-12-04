a34d331e5684b3cc925b71f9321afc7be10e5d58
Evgeny Kropotin <ekropotin@gmail.com>
2025-12-03 16:17:02 -0800

    day 2 part 2

--- day_2/main.py
+++ day_2/main.py
@@ -1,6 +1,20 @@
 import fileinput
 
 
+def has_repeats(s: str):
+    for seq_len in range(1, len(s) // 2 + 1):
+        if len(s) % seq_len != 0:
+            continue
+        match = s[0:seq_len]
+        i = seq_len
+        while True:
+            if s[i : i + seq_len] != match:
+                break
+            if i + seq_len == len(s):
+                return True
+            i += seq_len
+
+
 def main():
     res = 0
     with fileinput.input() as f:
@@ -10,13 +24,8 @@ def main():
             start, end = r.split("-")
             print(f"processing range: {start} - {end}")
             for i in range(int(start), int(end) + 1):
-                i_str = str(i)
-                if len(i_str) % 2 != 0:
-                    continue
-                split_idx = len(i_str) // 2
-                first_half = i_str[:split_idx]
-                second_half = i_str[split_idx:]
-                if first_half == second_half:
+                print(f"processing {i}")
+                if has_repeats(str(i)):
                     res += i
     print(res)
 
