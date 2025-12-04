e6925cbd1783c695200ec8d3b0f5942d4c12f79f
Evgeny Kropotin <ekropotin@gmail.com>
2025-12-03 21:20:11 -0800

    day 4 part 1

---
+++ day_4/main.py
@@ -0,0 +1,37 @@
+import fileinput
+
+
+def create_matrix():
+    res = []
+    with fileinput.input() as f:
+        for line in f:
+            res.append([ch for ch in line[:-1]])
+    return res
+
+
+def main():
+    matrix = create_matrix()
+    res = 0
+    rows, cols = len(matrix), len(matrix[0])
+    directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (1, -1), (-1, 1)]
+    for r in range(0, rows):
+        for c in range(0, cols):
+            if matrix[r][c] != "@":
+                continue
+            adj_count = 0
+            for d in directions:
+                new_r = r + d[0]
+                new_c = c + d[1]
+                if (
+                    new_r in range(0, rows)
+                    and new_c in range(0, cols)
+                    and matrix[new_r][new_c] == "@"
+                ):
+                    adj_count += 1
+            if adj_count < 4:
+                res += 1
+    print(res)
+
+
+if __name__ == "__main__":
+    main()
