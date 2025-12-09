7349d7a6bb854f328fa63b7bf0d96fb3979d8dbe
Evgeny Kropotin <ekropotin@gmail.com>
2025-12-08 17:26:07 -0800

    day 8 part 1

--- day_8/main.py
+++ day_8/main.py
@@ -1,55 +1,38 @@
 import fileinput
-import math
-
+import heapq
 from scipy.cluster.hierarchy import DisjointSet
 
 
 def main():
-    box_to_pos = {}
+    coordinates = []
     with fileinput.input() as f:
         for i, line in enumerate(f):
-            box_to_pos[i] = [int(coord) for coord in line.rstrip("\n").split(",")]
+            coordinates.append([int(coord) for coord in line.rstrip("\n").split(",")])
 
-    boxes_count = len(box_to_pos)
+    BOX_COUNT = len(coordinates)
+    disjoint_set = DisjointSet(range(0, BOX_COUNT))
 
-    disjoint_set = DisjointSet(range(0, boxes_count))
+    # minheap of distances
+    heap = []
+    for i in range(0, BOX_COUNT):
+        x1, y1, z1 = coordinates[i]
+        for j in range(i + 1, BOX_COUNT):
+            x2, y2, z2 = coordinates[j]
+            dist = (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
+            heapq.heappush(heap, (dist, i, j))
 
-    def calc_distance(id1: int, id2: int):
-        pos1 = box_to_pos[id1]
-        pos2 = box_to_pos[id2]
-        return math.sqrt(
-            (pos1[0] - pos2[0]) ** 2
-            + (pos1[1] - pos2[1]) ** 2
-            + (pos1[2] - pos2[2]) ** 2
-        )
+    MAX_CONNECTIONS = 1000
+    for i in range(0, MAX_CONNECTIONS):
+        _, box1, box2 = heapq.heappop(heap)
+        disjoint_set.merge(box1, box2)
 
-    connects_done = 0
-    CONN_LIMIT = 1000
-    while connects_done < CONN_LIMIT - 1:
-        closest_pair = None
-        best_distance = math.inf
-        for i in range(boxes_count):
-            for j in range(boxes_count):
-                if disjoint_set.connected(i, j):
-                    continue
-                dist = calc_distance(i, j)
-                if dist < best_distance:
-                    best_distance = dist
-                    closest_pair = (i, j)
-        if closest_pair:
-            connects_done += 1
-            print(f"connecting {closest_pair[0]} and {closest_pair[1]}")
-            disjoint_set.merge(closest_pair[0], closest_pair[1])
-            # print(disjoint_set.subsets())
-        else:
-            break
-
-    lens = [len(subs) for subs in disjoint_set.subsets()]
+    # print(disjoint_set.subsets())
+    lens = [len(subset) for subset in disjoint_set.subsets()]
     lens.sort(reverse=True)
-    acc = 1
-    for i in range(0, min(len(lens), 3)):
-        acc *= lens[i]
-    print(acc)
+    res = 1
+    for i in range(0, min(3, len(lens))):
+        res *= lens[i]
+    print(res)
 
 
 if __name__ == "__main__":
